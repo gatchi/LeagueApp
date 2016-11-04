59c2f1f3995428715c3492f5426e7e88436cbd32
gatchi <christen.got@gmail.com>
2016-11-04 16:40:21 -0700

    champ compare menu item now opens new activity

---
+++ src/main/java/com/gatchipatchi/LeagueApp/ChampCompareSetupActivity.java
@@ -0,0 +1,15 @@
+package com.gatchipatchi.LeagueApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class ChampCompareSetupActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.champ_compare_setup_activity);
+	}
+}
--- src/main/java/com/gatchipatchi/LeagueApp/MainActivity.java
+++ src/main/java/com/gatchipatchi/LeagueApp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TableLayout;
 import android.widget.ImageView.ScaleType;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
 import android.view.KeyEvent;
 import android.view.View;
@@ -193,6 +194,7 @@ public class MainActivity extends Activity
 		editText.setOnEditorActionListener(searchListener);
 	}
 	
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		MenuInflater inflater = getMenuInflater();
@@ -200,6 +202,13 @@ public class MainActivity extends Activity
 		return true;
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		Intent intent = new Intent(this, ChampCompareSetupActivity.class);
+		startActivity(intent);
+		return false;
+	}
 	
 	//----------------- Listeners ---------------------//
 	
